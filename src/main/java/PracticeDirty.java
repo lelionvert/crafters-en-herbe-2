797ca14059abd096d8913e514f436bd6199661d4
Thami Inaflas <inaflas.thami@gmail.com>
2019-08-26 23:36:32 +0200

    Added first elements of PracticeDirty

---
+++ src/main/java/PracticeDirty.java
@@ -0,0 +1,32 @@
+import java.util.Date;
+
+public class PracticeDirty {
+
+    public static void main(String[] args) {
+        System.out.println("This is a program, that's gonna do some stuff.");
+
+        System.out.println("Let's try writing up numbers from 1 to 10");
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+
+        for (int i = 6; i < 11; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("We want to have add values " +
+                "together and prints out the results.");
+
+        System.out.println("Adding 1 and 2 gives :" + 1 + 2);
+        System.out.println("Adding 3 and 4 gives :" + 3 + 4);
+
+        String z = "";
+        System.out.println("Adding x and y gives :" + z);
+
+
+
+    }
+}
